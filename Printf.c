4adacfcf2b008c8fa7e6be3cf9df843e9d6e9652
Abarna Pandian <abarnapandian2000@gmail.com>
2021-06-10 00:40:15 +0530

    Add files via upload

---
+++ Printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int two = 2, three = 3, six = 6;
+    int result = (two + three) * six / three;
+    printf("I am new to programming but i known how to do arithematic operations like this: (%d + %d) * %d / %d", two, three, six, three);
+    printf("\nResult is %d", result);
+
+} 
