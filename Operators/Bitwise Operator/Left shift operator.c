e6f93b28658f310cda0fd3367d17edaf4a9ea267
Abarna Pandian <abarnapandian2000@gmail.com>
2021-06-14 12:55:35 +0530

    Add files via upload

---
+++ Operators/Bitwise Operator/Left shift operator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    char var = 6;
+    printf("%d", var<<3);
+    return 0;
+}
---
+++ Operators/Bitwise Operator/XOR operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a = 4, b = 3;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("After XOR a = %d and b = %d", a, b);
+    return 0;
+
+}
