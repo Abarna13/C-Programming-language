95023ed453145f8e915f7347cf4991d222af2fd0
Abarna Pandian <abarnapandian2000@gmail.com>
2021-06-15 20:10:37 +0530

    Add files via upload

---
+++ Operators/Associativity of operators.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() 
+{
+    int a = 10, b = 20, c = 30, d = 40;
+    if(a <= b == d > c)
+    {
+        printf("True");
+        
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
---
+++ Operators/Associativity operator2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a =fun1() + fun2();
+    printf("\n%d", a);
+}
+
+
+
+
+
+int fun1()
+{
+    printf("Academy");
+    return 1;
+
+}
+
+
+int fun2()
+{
+    printf("Neso");
+    return 1;
+
+}
+
