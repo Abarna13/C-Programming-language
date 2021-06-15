5bb0df4b18f9c0146da00959e82c4865e8ad629a
Abarna Pandian <abarnapandian2000@gmail.com>
2021-06-16 00:22:00 +0530

    Add files via upload

---
+++ Conditionals/If-Else Condition/If-else2.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main()
+{
+    int n = 4;
+    if(n == 4)
+    {
+        printf("n is 4");
+    }
+    else
+    {
+        printf("n is not  4");
+    }
+    
+    
+    return 0;
+    
+}
