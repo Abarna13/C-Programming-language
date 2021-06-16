3ca4c11762f4154d9322281fbf0e5ccab87e0a14
Abarna Pandian <abarnapandian2000@gmail.com>
2021-06-16 21:08:29 +0530

    Add files via upload

---
+++ Conditionals/For Loop/For loop.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i = 3; i > 0; i--)
+    {
+        printf("Value of i is : %d\n", i);
+    }
+    return 0;
+}
---
+++ Conditionals/For Loop/For loop2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main()  
+{  
+    int a,b,c;  
+    for(a=0,b=12,c=23;a<2;a++)  
+    {  
+        printf("Value of a+b+c : %d\n", a+b+c);  
+    }  
+}  
