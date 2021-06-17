432eb87a5a79fa44e4d8df9e1f257786dacaf319
Abarna Pandian <abarnapandian2000@gmail.com>
2021-06-17 20:21:32 +0530

    Add files via upload

---
+++ Conditionals/Continue.c
@@ -0,0 +1,20 @@
+
+//Odd Numbers program 
+
+#include <stdio.h>
+ 
+int main ()
+{
+    int i, n=2;
+    
+    for(i = 1;i<=30;i++)
+    {
+        if(i == n)
+        {
+            n = n+2;
+            continue;
+        }
+        printf("Value of i : %d\n", i);
+    }
+ return 0;
+}
