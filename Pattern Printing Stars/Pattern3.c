cc5cf824a407e2c9585b1de783ee338683c4962a
Abarna Pandian <abarnapandian2000@gmail.com>
2021-06-19 00:12:53 +0530

    Add files via upload

---
+++ Pattern Printing Stars/Pattern3.c
@@ -0,0 +1,37 @@
+/* Program for printing stars
+
+
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+*/
+
+
+
+
+
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j; // i -> rows , j -> Columns
+    for(i=1; i<=5; i++)
+    {
+       for(j=5; j>=i; j--) 
+       {
+           printf("* ");
+       }
+       printf("\n");
+   }
+    return 0;
+}
+
+
+
