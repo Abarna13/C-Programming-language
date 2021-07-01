b325bb12922e0d5ab2037bc058dd4d0fb58d7086
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-01 21:51:14 +0530

    Update Sum of first n natural numbers.c

--- Programs/Sum of first n natural numbers.c
+++ Programs/Sum of first n natural numbers.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-    int n,i,sum=0;
-    sum = 0;
+    int n,i,sum = 0;
+    
     printf("\nEnter the Number : ");
     scanf("%d", &n);
     for(i=1;i<=n;i++)
     {
         sum = sum + i;
     }
-    printf("%d", sum);
+    printf("\nSum of %d Natural number is : %d", n, sum);
     return 0;
 
 
