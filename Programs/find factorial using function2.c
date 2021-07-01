36c78aba2e2662ee11b192bd105ee623278b2492
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-01 22:43:13 +0530

    Add files via upload

---
+++ Programs/find factorial using function2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int factorial(int input)
+{
+    int fact =1,i;
+    for(i=1;i<=input;i++)
+    {
+        fact = fact * i;
+        
+    }
+    return fact;
+    
+}
+
+
+//Main function
+
+int main()
+{
+    int first_number, second_number;
+    
+    printf("Enter the first number : ");
+    scanf("%d", &first_number);
+    
+    printf("Enter the second number : ");
+    scanf("%d", &second_number);
+    
+    printf("Factorial of %d is : %d", first_number, factorial(first_number));
+    printf("\nFactorial of %d is : %d", second_number, factorial(second_number));
+    
+}
+
+    
+    
+    
