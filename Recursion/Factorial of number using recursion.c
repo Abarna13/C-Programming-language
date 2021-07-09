b1b4ea452c05572c076d41312dd2c326b99907ee
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-10 00:10:21 +0530

    Add files via upload

---
+++ Recursion/Factorial of number using recursion.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n-1);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    printf("Factorial of %d is %d", n, factorial(n));
+    return 0;
+}
