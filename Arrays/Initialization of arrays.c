bf6959df09e84af1b3e35a2f33a5b64c3048248e
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-18 23:58:19 +0530

    Add files via upload

---
+++ Arrays/Initialization of arrays.c
@@ -0,0 +1,21 @@
+//Program for initialization of arrays
+
+
+#include<stdio.h>  
+int main()
+{
+    int i=0;
+    int marks[5];//declaration of array       
+    marks[0]=80;//initialization of array    
+    marks[1]=60;    
+    marks[2]=70;    
+    marks[3]=85;    
+    marks[4]=75;
+
+
+for(i=0;i<5;i++)
+{
+    printf("%d \n",marks[i]);    
+}    
+return 0;  
+}    
