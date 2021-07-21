a2f723a50881ad468f02f95b3a1479d18262c671
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-21 11:18:20 -0300

    Create One Dimensional Array  Program.c

---
+++ Arrays/One Dimensional Array  Program.c
@@ -0,0 +1,14 @@
+//Program for one dimensional array
+
+#include <stdio.h>
+
+int main() 
+{
+    int a[5] = {1,2,3,4,7};
+    int i;
+    for(i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+    
