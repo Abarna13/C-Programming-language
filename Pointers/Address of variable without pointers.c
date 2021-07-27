8f53808a22fd6a0c915781bd3205f30dee8be94c
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-27 23:42:15 +0530

    Rename Address of variable without pointers.c to Address of variable.c

--- Pointers/Address of variable without pointers.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int var = 7;
-    printf("Value of the variable var is: %d\n", var);
-    printf("Memory address of the variable var is: %p\n", &var);  //%p ->   used to printing the address of a pointer the addresses are depending by our system bit.
