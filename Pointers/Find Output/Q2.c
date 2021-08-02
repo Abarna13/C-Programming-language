7f39d095882dce0378d1fb300cec4033bd0fc76e
Abarna Pandian <abarnapandian2000@gmail.com>
2021-08-02 18:10:00 +0530

    Add files via upload

---
+++ Pointers/Find Output/Q2.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+
+    int a[] = {5,16,7,89,45,32,23,10};
+    int *p = &a[1];
+    int *q = &a[5];
+
+    printf("%d ",  *(p+3));
+    printf("%d ",  *(q-3));
+    printf("%d ",  q - p);
+    printf("%d ",  p < q);
+    printf("%d ",  *p < *q);
+    return 0;
+}
