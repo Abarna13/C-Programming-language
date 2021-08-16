76991c126548ea810cdffbb0806107b39fbb6ff6
Abarna Pandian <abarnapandian2000@gmail.com>
2021-08-16 13:53:35 +0530

    Add files via upload

---
+++ String Literals/Declaring and Initializing String Variables.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    char s[] = "Hello World Hi !!!!";
+    printf("%s", s);
+}
---
+++ String Literals/Splicing.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+
+    printf("%s", "You have to dream before your dreams can come true.   "
+           "\n--A.P.J.Abdul Kalam");
+}
