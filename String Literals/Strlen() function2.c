daa4f549d682ef42bb5ffa4bb77697c6ebc25c91
Abarna Pandian <abarnapandian2000@gmail.com>
2021-08-21 08:25:56 +0530

    Add files via upload

---
+++ String Literals/Strlen() function2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+
+    char str[] = "Hello Kitty cat";
+    printf("%d", strlen(str));
+    return 0;
+}
---
+++ String Literals/Strlen() function3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+
+    char str[49] = "Hello Anjana";
+    printf("%d", strlen(str));
+    return 0;
+}
